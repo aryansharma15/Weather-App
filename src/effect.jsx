e5f1d25a28dd1d7cce24c9beec7367e70519c75a
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-06-09 23:53:16 +0530

    called api

--- src/effect.jsx
+++ src/effect.jsx
@@ -3,7 +3,32 @@ import React, { useEffect, useState } from 'react'
 
 function Effect() {
 
-  const [type, setType] = useState("post")
+  const [type, setType] = useState("Zeus")
+  
+  const [weather, setWeather] = useState({});
+  const [locations, setLocations] = useState("Madrid");
+  const [landscape, setLandscape] = useState([]);
+
+  function Loc() {
+    fetch(
+       `http://api.openweathermap.org/data/2.5/weather?q=${locations}&APPID={API_KEY_FOR_WEATHER_API}&units=metric`
+    )
+
+    .then((res) => {
+      if(res.ok) {
+        console.log(res.status);
+        return res.json();
+      }
+      else{
+        if(res.status === 404){
+          return alert("Error! Wrong Location");
+
+        }
+        alert("Error!");
+        throw new Error("You have an error");
+      }
+    })
+  }
 
   useEffect(() => {
     console.log("Render");
@@ -11,9 +36,6 @@ function Effect() {
 
   return (
     <div>
-      <button onClick={setType("Zeus")}>Zeus</button>
-      <button onClick={setType("Poseidon")}>Poseidon</button>
-      <button onClick={setType("Hades")}>Hades</button>
 
       <h1>{type}</h1>
     </div>
