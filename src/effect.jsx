7d3d21952610562c7d6316c4a7e9a43e4f4eb443
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-06-10 23:34:26 +0530

    retrieving results

--- src/effect.jsx
+++ src/effect.jsx
@@ -14,26 +14,46 @@ function Effect() {
        `http://api.openweathermap.org/data/2.5/weather?q=${locations}&APPID={API_KEY_FOR_WEATHER_API}&units=metric`
     )
 
-    // For error or bad response
-    .then((res) => {
-      if(res.ok) {
-        console.log(res.status);
-        return res.json();
-      }
-      else{
-        if(res.status === 404){
-          return alert("Error! Wrong Location");
-
+      // For error or bad response
+      .then((res) => {
+        if(res.ok) {
+          console.log(res.status);
+          return res.json();
         }
-        alert("Error!");
-        throw new Error("You have an error");
-      }
-    })
+        else{
+          if(res.status === 404){
+            return alert("Error! Wrong Location");
 
-    // for approved
-    .then(() => {
+          }
+          alert("Error!");
+          throw new Error("You have an error");
+        }
+      })
 
-    })
+      // for approved
+      .then(() => {
+        setWeather(object);
+        console.log(weather);
+      })
+      .catch((error) => console.log(error));
+
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${locations}&client_id={API_KEY_FOR_UNSPLASH}`
+    )
+      .then((res) => {
+        if(res.ok){
+          return res.json;
+        }
+        else{
+          throw new Error("You made an error");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setLandscape(data?.results[0]?.urls?.raw);
+      })
+      .catch((error) => console.log(error));
+
   }
 
   useEffect(() => {
